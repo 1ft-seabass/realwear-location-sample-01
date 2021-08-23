5657f21618d8e22054dbbf52955cf0c0024c3ca2
Tanaka Seigo <seigo@1ft-seabass.jp>
2021-08-23 11:41:13 +0900

    first commit

---
+++ Assets/Scripts/LogUIMessage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogUIMessage : MonoBehaviour
+{
+    TextMesh UIText = null;
+
+    string messages = "";
+
+    void Start()
+    {
+        UIText = GameObject.Find("LogMessageText").GetComponent<TextMesh>();
+
+        Application.logMessageReceived += logMessageReceived;
+    }
+
+    private void logMessageReceived(string condition, string stackTrace, LogType type)
+    {
+        messages = condition + "\n" + messages;
+
+        UIText.text = messages;
+
+        switch (type)
+        {
+            case LogType.Error:
+                break;
+            case LogType.Assert:
+                break;
+            case LogType.Warning:
+                break;
+            case LogType.Log:
+                break;
+            case LogType.Exception:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    void Start()
+    {
+        Invoke("Launch", 1.0f);
+    }
+
+    void Launch()
+    {
+        Debug.Log("Main Launched!");
+
+        StartCoroutine(StartLocationService());
+    }
+
+    IEnumerator StartLocationService()
+    {
+        // Refere from https://docs.unity3d.com/jp/current/ScriptReference/LocationService.Start.html
+
+        //////////////////////////////////////////////////////////////////////
+        
+        // First, check if user has location service enabled
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("location is disabled by user");
+
+            yield break;
+        }
+            
+
+        // Start service before querying location
+        Input.location.Start();
+
+        // Wait until service initializes
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            Debug.Log("wait " + maxWait + " s");
+            Debug.LogFormat("status: {0}", Input.location.status);
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        // Service didn't initialize in 20 seconds
+        if (maxWait < 1)
+        {
+            Debug.Log("Timed out");
+            yield break;
+        }
+
+        // Connection has failed
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.Log("Unable to determine device location");
+            yield break;
+        }
+        else
+        {
+            // Access granted and location value could be retrieved
+            Debug.Log("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+        }
+
+        // Stop service if there is no need to query location updates continuously
+        Input.location.Stop();
+    }
+}
